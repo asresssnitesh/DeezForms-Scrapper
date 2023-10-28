ac1450ea417dd1aea968cb2c1bc1e2a13fc538ed
lokeshwarlakhi <lokeshwarlakhi@gmail.com>
2023-10-29 01:32:26 +0530

    linkedin scrapper ready v.0.0

--- deezforms.py
+++ deezforms.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
 from creds import email,password
 import deezforms_login as dl
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Chrome()
 linkedin_email = email
 linkedin_password = password
 dl.login(driver, email, password)
+wait = WebDriverWait(driver, 10)
+element = wait.until(EC.visibility_of_element_located((By.ID, 'element_id')))
 print("logged in")
-# person = Person("https://www.linkedin.com/in/sriharshamalla/", driver=driver)
+
 
 
